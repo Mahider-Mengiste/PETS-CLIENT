b570fcdd32a50e224628bcfb510fe19df2396f41
mmengi18 <mmengi18@montgomerycollege.edu>
2022-07-29 11:56:02 -0400

    added PetsIndex component

--- PetsApp/pets-frontend/pets-frontend/src/components/Home.js
+++ PetsApp/pets-frontend/pets-frontend/src/components/Home.js
@@ -1,9 +1,11 @@
+import PetsIndex from './pets/petsIndex'
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
 	return (
 		<>
+			<PetsIndex />
 			<h2>Home Page</h2>
 		</>
 	)
